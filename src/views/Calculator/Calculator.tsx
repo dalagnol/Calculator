40f55b52bf3959573ee763f983153c1296fe8d87
Guilherme Moresco <guilherme.moresco@vortigo.com.br>
2020-04-12 16:05:14 -0300

    code rev 1

--- src/views/Calculator/Calculator.tsx
+++ src/views/Calculator/Calculator.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback, useMemo } from "react";
 import { useTheme } from "../../themes";
 import Operations from "../../operations";
 
 import { Container, Screen, Result } from "./styles";
 import { Button } from "../../components";
 import { theme } from "./json";
-import { keyCodes } from "./constants";
+import { keyCodes, format as formatted, table, opcodes } from "./constants";
 
 export default function Calculator() {
   useTheme("calculator", theme);
+  const ref: any = useRef(null);
 
   const [editing, setEditing] = useState("x");
   const [x, setX] = useState(0);
@@ -59,40 +60,43 @@ export default function Calculator() {
     }
   };
 
-  const submitNumber = (value: string) => {
-    let result = "";
+  const submitNumber = useCallback(
+    (value: string) => {
+      let result = "";
 
-    if (row || editing === "y") {
-      result = String(y);
-      setEditing("y");
-    } else if (!newNumber && editing === "x") {
-      result = String(x);
-    } else {
-      setX(0);
-      result = "0";
-    }
-
-    if (result === "0") {
-      result = "";
-    }
-
-    if (comma === 2) {
-      result = result.concat(`.${value}`);
-      setComma(0);
-    } else if (result.length < 9) {
-      result = result.concat(value);
-      setNewNumber(false);
-    }
-
-    if (row || editing === "y") {
-       setY(Number(result));
-       setDisplayY(true);
+      if (row || editing === "y") {
+        result = String(y);
+        setEditing("y");
+      } else if (!newNumber && editing === "x") {
+        result = String(x);
       } else {
-       setX(Number(result));
+        setX(0);
+        result = "0";
       }
 
-    return result;
-  };
+      if (result === "0") {
+        result = "";
+      }
+
+      if (comma === 2) {
+        result = result.concat(`.${value}`);
+        setComma(0);
+      } else if (result.length < 9) {
+        result = result.concat(value);
+        setNewNumber(false);
+      }
+
+      if (row || editing === "y") {
+        setY(Number(result));
+        setDisplayY(true);
+      } else {
+        setX(Number(result));
+      }
+
+      return result;
+    },
+    [comma, editing, newNumber, row, x, y]
+  );
 
   const submitOperation = (op: string) => {
     if (!x) {
@@ -120,223 +124,133 @@ export default function Calculator() {
     setComma(result);
   };
 
-  const prepare = (n: number) => {
-    let result = String(n);
-    
-    if (result.includes(".")) {
-      result = result.replace(".", ",");
-    }
-
-    if (n >= 1000 && n <= 9999) {
-      let part1 = result.substring(0, 1);
-      let part2 = result.substring(1);
-
-      result = `${part1}.${part2}`;
-    } else if (n >= 10000 && n <= 99999) {
-      let part1 = result.substring(0, 2);
-      let part2 = result.substring(2);
-
-      result = `${part1}.${part2}`;
-    } else if (n >= 100000 && n <= 999999) {
-      let part1 = result.substring(0, 3);
-      let part2 = result.substring(3);
-
-      result = `${part1}.${part2}`;
-    } else if (n >= 1000000 && n <= 9999999) {
-      let part1 = result.substring(0, 1);
-      let part2 = result.substring(1, 4);
-      let part3 = result.substring(4);
-
-      result = `${part1}.${part2}.${part3}`;
-    } else if (n >= 10000000 && n <= 99999999) {
-      let part1 = result.substring(0, 2);
-      let part2 = result.substring(2, 5);
-      let part3 = result.substring(5);
-
-      result = `${part1}.${part2}.${part3}`;
-    } else if (n >= 100000000 && n <= 999999999) {
-      let part1 = result.substring(0, 3);
-      let part2 = result.substring(3, 6);
-      let part3 = result.substring(6);
-
-      result = `${part1}.${part2}.${part3}`;
-    };
-
-    return result;
-  };
-
-  const keyHandler = (e: any) => {
-    if (e.keyCode >= 48 && e.keyCode <= 57) {
-      submitNumber(keyCodes[e.keyCode]);
-    } else if (e.keyCode === 188 || e.keyCode === 190) {
-      submitComma();
-    } else if (e.keyCode === 189 || e.keyCode === 170 || e.keyCode === 191) {
-      submitOperation(keyCodes[e.keyCode])
-    } else if (e.shiftKey && e.keyCode === 187) {
-      submitOperation("sum");
-    } else if (e.shiftKey && e.keyCode === 56) {
-      submitOperation("multiply");
-    } else if (e.keyCode === 187 || e.keyCode === 13) {
+  const handlers: { [x: number]: Function } = {
+    188: submitComma,
+    190: submitComma,
+    27: clear,
+    67: clear,
+    13() {
       operateTwoNumbers(x, y, operation);
       setEditing("x");
-    } else if (e.keyCode === 27 || e.keyCode === 67) {
-      clear();
-    } else if (e.keyCode === 8) {
+    },
+    53(e: any) {
+      if (e.shiftKey) {
+        operateOneNumber("percentage");
+      } else {
+        submitNumber("5");
+      }
+    },
+    56(e: any) {
+      if (e.shiftKey) {
+        submitOperation("multiply");
+      } else {
+        submitNumber("8");
+      }
+    },
+    189(e: any) {
+      if (e.shiftKey) {
+        operateOneNumber("change");
+      } else {
+        submitOperation("subtract");
+      }
+    },
+    191() {
+      submitOperation("divide");
+    },
+    8(e: any) {
       const res = keyCodes[e.keyCode](editing === "x" ? x : y);
       editing === "x" ? setX(res) : setY(res);
+    },
+    187(e: any) {
+      if (e.shiftKey) {
+        submitOperation("sum");
+      } else {
+        operateTwoNumbers(x, y, operation);
+        setEditing("x");
+      }
+    },
+  };
+
+  const handle = useCallback(
+    (e: any) => {
+      if (handlers[e.keyCode]) {
+        handlers[e.keyCode](e);
+      } else if (e.keyCode >= 48 && e.keyCode <= 57 && !e.shiftKey) {
+        submitNumber(keyCodes[e.keyCode]);
+      }
+    },
+    [handlers, submitNumber]
+  );
+
+  const e = (keyCode: number, shiftKey = false) => ({ keyCode, shiftKey });
+
+  const Buttons = useMemo(() => {
+    const result = [];
+    let i = 0;
+
+    let zero = false;
+
+    for (let row = 2; row <= 6; row++) {
+      for (let column = 1; column <= 4; column++) {
+        if (i < table.length) {
+          let justPassedZero = false;
+
+          if (!zero) {
+            zero = table[i][0] === "0";
+            if (zero) {
+              justPassedZero = true;
+            }
+          }
+
+          const [key, code, shift]: [
+            string,
+            number,
+            boolean | undefined
+          ] = table[i++];
+
+          result.push(
+            <Button
+              color={
+                column === 4 && operation === opcodes[row]
+                  ? "selected"
+                  : column === 4
+                  ? "main"
+                  : row === 2
+                  ? "secondary"
+                  : "number"
+              }
+              position={{ column, row, zero }}
+              onClick={() => handle(e(code, shift))}
+            >
+              {key}
+            </Button>
+          );
+
+          if (justPassedZero) {
+            column++;
+          }
+        }
+      }
     }
-  }
-    
+
+    return result;
+  }, [operation, handle]);
+
   return (
     <Container>
       <Screen>
-        <Result>{editing === "y" && displayY ? prepare(y) : prepare(x)}</Result>
+        <Result>
+          {editing === "y" && displayY ? formatted(y) : formatted(x)}
+        </Result>
       </Screen>
-      <Button color={"secondary"} position={{column: 1, row: 2}} onClick={() => clear()} onKeyUp={keyHandler}>
-        C
-      </Button>
-      <Button
-        color={"secondary"}
-        position={{column: 2, row: 2}}
-        onClick={() => operateOneNumber("change")}
-        onKeyUp={keyHandler}
-      >
-        ±
-      </Button>
-      <Button
-        color={"secondary"}
-        position={{column: 3, row: 2}}
-        onClick={() => operateOneNumber("percentage")}
-        onKeyUp={keyHandler}
-      >
-        %
-      </Button>
-      <Button
-        color={operation === "divide" ? "selected" : "main"}
-        position={{column: 4, row: 2}}
-        onClick={() => submitOperation("divide")}
-        onKeyUp={keyHandler}
-      >
-        ÷
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 1, row: 3}}
-        onClick={() => submitNumber("7")}
-        onKeyUp={keyHandler}
-      >
-        7
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 2, row: 3}}
-        onClick={() => submitNumber("8")}
-        onKeyUp={keyHandler}
-      >
-        8
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 3, row: 3}}
-        onClick={() => submitNumber("9")}
-        onKeyUp={keyHandler}
-      >
-        9
-      </Button>
-      <Button
-        color={operation === "multiply" ? "selected" : "main"}
-        position={{column: 4, row: 3}}
-        onClick={() => submitOperation("multiply")}
-        onKeyUp={keyHandler}
-      >
-        ×
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 1, row: 4}}
-        onClick={() => submitNumber("4")}
-        onKeyUp={keyHandler}
-      >
-        4
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 2, row: 4}}
-        onClick={() => submitNumber("5")}
-        onKeyUp={keyHandler}
-      >
-        5
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 3, row: 4}}
-        onClick={() => submitNumber("6")}
-        onKeyUp={keyHandler}
-      >
-        6
-      </Button>
-      <Button
-        color={operation === "subtract" ? "selected" : "main"}
-        position={{column: 4, row: 4}}
-        onClick={() => submitOperation("subtract")}
-        onKeyUp={keyHandler}
-      >
-        -
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 1, row: 5}}
-        onClick={() => submitNumber("1")}
-        onKeyUp={keyHandler}
-      >
-        1
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 2, row: 5}}
-        onClick={() => submitNumber("2")}
-        onKeyUp={keyHandler}
-      >
-        2
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 3, row: 5}}
-        onClick={() => submitNumber("3")}
-        onKeyUp={keyHandler}
-      >
-        3
-      </Button>
-      <Button
-        color={operation === "sum" ? "selected" : "main"}
-        position={{column: 4, row: 5}}
-        onClick={() => submitOperation("sum")}
-        onKeyUp={keyHandler}
-      >
-        +
-      </Button>
-      <Button
-        color={"number"}
-        position={{column: 1, row: 6, zero: true}}
-        onClick={() => submitNumber("0")}
-        onKeyUp={keyHandler}
-      >
-        0
-      </Button>
-      <Button color={"number"} position={{column: 3, row: 6}} onClick={() => submitComma()} onKeyUp={keyHandler}>
-        ,
-      </Button>
-      <Button
-        color={"main"}
-        position={{column: 4, row: 6}}
-        onClick={() => {
-          operateTwoNumbers(x, y, operation);
-          setEditing("x");
-        }}
-        onKeyUp={keyHandler}
-      >
-        =
-      </Button>
+      <input
+        ref={ref}
+        autoFocus={true}
+        style={{ opacity: 0 }}
+        onBlur={() => ref.current?.focus()}
+        onKeyDown={handle}
+      />
+      {Buttons}
     </Container>
   );
 }
--- src/views/Calculator/constants.ts
+++ src/views/Calculator/constants.ts
@@ -1,3 +1,29 @@
+type flag = "en" | "pt" | "fr";
+type spot = "dec" | "sep";
+type character = "," | "." | " ";
+
+const locale: {
+  [x in flag]: {
+    [y in spot]: character;
+  };
+} = {
+  en: {
+    dec: ",",
+    sep: ".",
+  },
+  pt: {
+    dec: ".",
+    sep: ",",
+  },
+  fr: {
+    dec: " ",
+    sep: ",",
+  },
+};
+
+const localised: { [y in spot]: character } =
+  locale[(process.argv[3] as flag) || "en"];
+
 export const keyCodes: {
   [x: number]: any;
 } = {
@@ -20,5 +46,53 @@ export const keyCodes: {
   170: "multiply",
   187: "operate",
   189: "subtract",
-  191: "divide"
+  191: "divide",
 };
+
+export function format(x: number | string) {
+  x = String(x);
+  let y = "";
+
+  if (x.includes(".")) {
+    y = x.substring(x.indexOf(".") + 1);
+    x = x.substring(0, x.indexOf("."));
+  }
+
+  const arr = [];
+  for (let i = x.length - 3; i >= -2; i -= 3) {
+    arr.push(x.substring(i, i + 3));
+  }
+
+  arr.reverse();
+
+  return `${arr.join(localised.dec)}${Number(y) ? `${localised.sep}${y}` : ""}`;
+}
+
+export const opcodes: { [x: number]: string } = {
+  2: "divide",
+  3: "multiply",
+  4: "subtract",
+  5: "sum",
+};
+
+export const table: any = [
+  ["C", 27],
+  ["±", 189, true],
+  ["%", 53, true],
+  ["÷", 191],
+  ["7", 55],
+  ["8", 56],
+  ["9", 57],
+  ["×", 56, true],
+  ["4", 52],
+  ["5", 53],
+  ["6", 54],
+  ["-", 189],
+  ["1", 49],
+  ["2", 50],
+  ["3", 51],
+  ["+", 187, true],
+  ["0", 48],
+  [",", 190],
+  ["=", 187],
+];
